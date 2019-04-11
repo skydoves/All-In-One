151ffe8880e1efad15d2beba66ef8f46bd8a1c72
skydoves <skydoves@naver.com>
2019-04-11 12:43:40 +0900

    migrate to androidX

--- app/src/main/java/com/skydoves/allinone/MainActivity.kt
+++ app/src/main/java/com/skydoves/allinone/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.skydoves.allinone
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
