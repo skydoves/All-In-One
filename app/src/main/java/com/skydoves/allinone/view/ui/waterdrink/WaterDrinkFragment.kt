5737ba569c80d8a93ad4e1980cc4db47c9eca5d2
skydoves <skydoves@naver.com>
2019-04-15 10:33:55 +0900

    added main fragments

---
+++ app/src/main/java/com/skydoves/allinone/view/ui/waterdrink/WaterDrinkFragment.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2019 skydoves
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.skydoves.allinone.view.ui.waterdrink
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.skydoves.allinone.view.ui.main.MainActivityViewModel
+import dagger.android.support.AndroidSupportInjection
+import javax.inject.Inject
+
+class WaterDrinkFragment : Fragment() {
+
+  @Inject
+  lateinit var viewModelFactory: ViewModelProvider.Factory
+  private lateinit var viewModel: MainActivityViewModel
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    initializeUI()
+  }
+
+  override fun onAttach(context: Context) {
+    AndroidSupportInjection.inject(this)
+    super.onAttach(context)
+  }
+
+  private fun initializeUI() {
+  }
+}
